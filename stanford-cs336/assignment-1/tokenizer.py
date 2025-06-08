ce58f7e91126a23662bf9f04e5068762d17bb9b1
Athe-kunal <athekunal@gmail.com>
2025-06-08 01:00:11 -0700

    tokenizer working

--- stanford-cs336/assignment-1/tokenizer.py
+++ stanford-cs336/assignment-1/tokenizer.py
@@ -1,13 +1,17 @@
 import collections
 import concurrent.futures
 import regex as re
+from tqdm import tqdm
 from typing import TypeAlias
+
 
 BytesTuple: TypeAlias = tuple[int, ...]
 BytesCount: TypeAlias = int
-VocabDict: TypeAlias = collections.defaultdict[BytesTuple, BytesCount]
-TokenID: TypeAlias = int
 
+VocabDict: TypeAlias = collections.defaultdict[BytesTuple, BytesCount]
+MergesDict: TypeAlias = list[tuple[int, ...]]
+TokenID: TypeAlias = int
+BASE_VOCAB_SIZE = 256
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 txt_file = "/Users/athekunal/Desktop/Stanford-cs336/Stanford-cs336-learning/stanford-cs336/data/TinyStoriesV2-GPT4-valid.txt"
 
@@ -15,8 +19,8 @@ txt_file = "/Users/athekunal/Desktop/Stanford-cs336/Stanford-cs336-learning/stan
 def convert_to_bytes(
     text: str,
 ) -> collections.defaultdict[BytesTuple, BytesCount]:
-    bytes_dict: collections.defaultdict[BytesTuple, BytesCount] = collections.defaultdict(
-        int
+    bytes_dict: collections.defaultdict[BytesTuple, BytesCount] = (
+        collections.defaultdict(int)
     )
     for match in list(re.finditer(PAT, text)):
         bytes_key = tuple(list(match.group().encode("utf-8")))
@@ -42,15 +46,60 @@ def read_until_eot(filepath, delimiter="<|endoftext|>", chunk_size=1024):
                 yield buffer[:idx]
                 buffer = buffer[idx + len(delimiter) :]
 
-def get_new_vocab_dict(vocab_dict: VocabDict, curr_max_vocab: int) -> VocabDict:
-    successive_dict: VocabDict = collections.defaultdict(int)
-    for k, v in vocab_dict.items():
-        for i in range(len(k)-1):
-            successive_dict[(k[i],k[i+1])]+=v
+
+def replace_all_subsequences(
+    a: tuple[int, ...], b: tuple[int, ...], curr_max_vocab: int
+) -> tuple[int, ...]:
+    if not b:
+        return a
+    n, m = len(a), len(b)
+    result = []
+    i = 0
+    while i <= n - m:
+        if a[i : i + m] == b:
+            result.append(curr_max_vocab)
+            i += m
+        else:
+            result.append(a[i])
+            i += 1
+    result.extend(a[i:])
+    return tuple(result)
+
+
+def get_new_vocab_dict(
+    curr_vocab_dict: VocabDict, curr_max_vocab: int
+) -> tuple[VocabDict, tuple[int, ...]]:
+    successive_dict: collections.defaultdict[tuple[int, ...], int] = (
+        collections.defaultdict(int)
+    )
+    for k, v in curr_vocab_dict.items():
+        for i in range(len(k) - 1):
+            successive_dict[(k[i], k[i + 1])] += v
+
     top1 = max(successive_dict.items(), key=lambda item: item[1])
-    combined_vocab_dict: VocabDict = collections.defaultdict(int)
-    for k,v in vocab_dict.items():
-        
+    combined_vocab_dict: VocabDict = collections.defaultdict()
+    for k, v in curr_vocab_dict.items():
+        new_key = replace_all_subsequences(k, top1[0], curr_max_vocab=curr_max_vocab)
+        combined_vocab_dict[new_key] = v
+    return combined_vocab_dict, top1[0]
+
+
+def unroll_merges(
+    vocab_dict: collections.defaultdict[TokenID, BytesTuple],
+    curr_merges: tuple[int, ...],
+    initial_vocab_len: int,
+) -> tuple[int, ...]:
+    actual_token_ids: list[int] = []
+
+    for cm in curr_merges:
+        assert cm in vocab_dict, f"Could not find the token id {cm}"
+        if cm >= initial_vocab_len - 1:
+            token_ids = vocab_dict[cm]
+            actual_token_ids.extend(token_ids)
+        else:
+            actual_token_ids.append(cm)
+    return tuple(actual_token_ids)
+
 
 def train_bpe(
     input_path: str,
@@ -58,17 +107,20 @@ def train_bpe(
     special_tokens: list[str],
     delimiter: str = "<|endoftext|>",
     chunk_size: int = 1024,
-) -> None:
-    # ) -> tuple[collections.defaultdict[int, int], list[tuple[bytes, bytes]]]:
-    initial_vocab = list(range(256)) + [256 + i for i in range(len(special_tokens))]
-    vocab_dict: collections.defaultdict[TokenID, BytesTuple] = collections.defaultdict(BytesTuple)
+) -> tuple[collections.defaultdict[TokenID, BytesTuple], MergesDict, dict[str, int]]:
+    initial_vocab = list(range(BASE_VOCAB_SIZE)) + [
+        BASE_VOCAB_SIZE + i for i in range(len(special_tokens))
+    ]
+    vocab_dict: collections.defaultdict[TokenID, BytesTuple] = collections.defaultdict(
+        BytesTuple
+    )
     for iv in initial_vocab:
         vocab_dict[iv] = (iv,)
-    special_tokens_map = {st: 256 + i for i, st in enumerate(special_tokens)}
+    special_tokens_map = {
+        st: BASE_VOCAB_SIZE + i for i, st in enumerate(special_tokens)
+    }
     num_merges = vocab_size - len(initial_vocab)
-    all_bytes_dict: VocabDict = (
-        collections.defaultdict(int)
-    )
+    all_bytes_dict: VocabDict = collections.defaultdict(int)
     with concurrent.futures.ProcessPoolExecutor() as executor:
         results = list(
             executor.map(
@@ -81,10 +133,21 @@ def train_bpe(
         for res in results:
             for key, val in res.items():
                 all_bytes_dict[key] += val
+    merges: list[tuple[int, ...]] = []
+    for merge in tqdm(range(num_merges)):
+        curr_max_vocab = BASE_VOCAB_SIZE + len(special_tokens) + merge
+        all_bytes_dict, curr_merges = get_new_vocab_dict(
+            all_bytes_dict, curr_max_vocab=curr_max_vocab
+        )
+        vocab_dict[curr_max_vocab] = unroll_merges(
+            vocab_dict, curr_merges, len(initial_vocab)
+        )
+        merges.append(curr_merges)
 
-    for merge in range(num_merges):
-
+    return vocab_dict, merges, special_tokens_map
 
 
 if __name__ == "__main__":
-    train_bpe(txt_file, 100, ["endoftext"])
+    vocab_dict, merges, special_tokens_map = train_bpe(txt_file, 500, ["<|endoftext|>"])
+    print(vocab_dict)
+    print(merges)
