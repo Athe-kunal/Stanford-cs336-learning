8bdbfdd2761960fc6ddabb5294d781e2d979aa79
Athe-kunal <athekunal@gmail.com>
2025-06-08 16:19:22 -0700

    parallelism

--- stanford-cs336/assignment-1/tokenizer.py
+++ stanford-cs336/assignment-1/tokenizer.py
@@ -2,9 +2,14 @@ import collections
 import concurrent.futures
 import regex as re
 import json
+import os
 from tqdm import tqdm
-from typing import TypeAlias
+from typing import TypeAlias, Generator, Any
 
+BASE_VOCAB_SIZE = 256
+PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+_PAT = re.compile(PAT)
+TXT_FILE = "/Users/athekunal/Desktop/Stanford-cs336/Stanford-cs336-learning/stanford-cs336/data/TinyStoriesV2-GPT4-valid.txt"
 
 BytesTuple: TypeAlias = tuple[int, ...]
 BytesCount: TypeAlias = int
@@ -12,10 +17,6 @@ BytesCount: TypeAlias = int
 VocabDict: TypeAlias = collections.defaultdict[BytesTuple, BytesCount]
 MergesDict: TypeAlias = list[tuple[int, ...]]
 TokenID: TypeAlias = int
-BASE_VOCAB_SIZE = 256
-PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
-_PAT = re.compile(PAT)
-txt_file = "/Users/athekunal/Desktop/Stanford-cs336/Stanford-cs336-learning/stanford-cs336/data/TinyStoriesV2-GPT4-valid.txt"
 
 
 def convert_to_bytes(
@@ -67,22 +68,63 @@ def replace_all_subsequences(
     return tuple(result)
 
 
+def _count_successive_pairs(bytes_key: BytesTuple, bytes_cnt: BytesCount) -> VocabDict:
+    aggr_successive_dict: VocabDict = collections.defaultdict(int)
+    for i in range(len(bytes_key) - 1):
+        aggr_successive_dict[(bytes_key[i], bytes_key[i + 1])] += bytes_cnt
+    return aggr_successive_dict
+
+
+def _replace_and_keep_cnt(
+    k: BytesTuple, top1: BytesTuple, curr_max_vocab: int, cnt: int
+) -> VocabDict:
+    new_key = replace_all_subsequences(a=k, b=top1, curr_max_vocab=curr_max_vocab)
+    result = collections.defaultdict(int)
+    result[new_key] = cnt
+    return result
+
+
+# at module scope
+def _count_successive_pairs_wrapper(arg):
+    bytes_key, bytes_cnt = arg
+    return _count_successive_pairs(bytes_key, bytes_cnt)
+
+
+def _replace_and_keep_wrapper(arg):
+    k, cnt, best_pair, new_id = arg
+    return replace_all_subsequences(k, best_pair, new_id), cnt
+
+
+def get_chunksize(items: list[Any]) -> int:
+    total_cpu_count = min(os.cpu_count() or 4, len(items))
+    chunk_size = len(items) // total_cpu_count
+    return max(1, chunk_size)
+
+
 def get_new_vocab_dict(
     curr_vocab_dict: VocabDict, curr_max_vocab: int
 ) -> tuple[VocabDict, tuple[int, ...]]:
-    successive_dict: collections.defaultdict[tuple[int, ...], int] = (
-        collections.defaultdict(int)
-    )
-    for k, v in curr_vocab_dict.items():
-        for i in range(len(k) - 1):
-            successive_dict[(k[i], k[i + 1])] += v
+    successive_dict: VocabDict = collections.defaultdict(int)
+    items = list(curr_vocab_dict.items())
+    chunk_size = get_chunksize(items=items)
+    args_iter_pairs = ((bk, bcnt) for bk, bcnt in items)
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for partial in executor.map(
+            _count_successive_pairs_wrapper, args_iter_pairs, chunksize=chunk_size
+        ):
+            for pair, cnt in partial.items():
+                successive_dict[pair] += cnt
 
-    top1 = max(successive_dict.items(), key=lambda item: item[1])
+    top1, top1_count = max(successive_dict.items(), key=lambda item: item[1])
     combined_vocab_dict: VocabDict = collections.defaultdict(int)
-    for k, v in curr_vocab_dict.items():
-        new_key = replace_all_subsequences(k, top1[0], curr_max_vocab=curr_max_vocab)
-        combined_vocab_dict[new_key] += v
-    return combined_vocab_dict, top1[0]
+
+    args_iter = ((k, v, top1, curr_max_vocab) for k, v in items)
+    with concurrent.futures.ProcessPoolExecutor() as executor:
+        for new_key, cnt in executor.map(
+            _replace_and_keep_wrapper, args_iter, chunksize=chunk_size
+        ):
+            combined_vocab_dict[new_key] += cnt
+    return combined_vocab_dict, top1
 
 
 def unroll_merges(
@@ -159,5 +201,5 @@ def save_dict_to_json(data: dict, file_path: str, indent: int = 4) -> None:
 
 
 if __name__ == "__main__":
-    vocab_dict, merges, special_tokens_map = train_bpe(txt_file, 500, ["<|endoftext|>"])
-    save_dict_to_json(vocab_dict, "temp.json", indent=1)
+    vocab_dict, merges, special_tokens_map = train_bpe(TXT_FILE, 500, ["<|endoftext|>"])
+    save_dict_to_json(vocab_dict, "temp2.json", indent=1)
