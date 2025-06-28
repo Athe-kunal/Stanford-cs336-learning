427cfa8d4020f9929d94642b6235a1f23b9c7020
Athe-kunal <athekunal@gmail.com>
2025-06-28 01:41:05 -0700

    tokenizer working

--- stanford-cs336/assignment_1/tokenizer.py
+++ stanford-cs336/assignment_1/tokenizer.py
@@ -1,29 +1,49 @@
 from __future__ import annotations
 import json
 import regex as re
-from typing import Iterable
 
 
 class Tokenizer:
     def __init__(
         self,
-        tokenizer_map: dict[tuple[int, ...], int],
+        tokens_to_ids_map: dict[tuple[int, ...], int],
+        ids_to_tokens_map: dict[int, list[int]],
         special_tokens: list[str],
         token_re: re.Pattern,
     ) -> None:
-        self.tokenizer_map = tokenizer_map
+        self.tokens_to_ids_map = tokens_to_ids_map
         self.token_re = token_re
         self.special_tokens = special_tokens
+        self.ids_to_tokens_map = ids_to_tokens_map
 
-    # def encode(self, text: str) -> list[int]:
-    #     bytes_list: list[int] = []
-    #     for match_ in re.finditer(self.token_re, text):
-    #         bytes_list.extend(match_.group().encode("utf-8"))
-    #     token_ids: list[int] = []
-    #     aggr_ids: list[int] = []
-    #     temp_ids: list[int] = []
-    #     for bl in bytes_list:
-    #         if (bl) in self.tokenizer_map
+    def encode(self, text: str) -> list[int]:
+        bytes_list: list[int] = []
+        for match_ in re.finditer(self.token_re, text):
+            bytes_list.extend(match_.group().encode("utf-8"))
+        if not bytes_list:
+            return []
+        merged_list: list[int] = []
+        encoded_list: list[int] = []
+        for bl in bytes_list:
+            merged_list.append(bl)
+            if tuple(merged_list) not in self.tokens_to_ids_map:
+                encoded_list.append(self.tokens_to_ids_map[tuple(merged_list[:-1])])
+                merged_list = [bl]
+        if tuple(merged_list) in self.tokens_to_ids_map:
+            encoded_list.append(self.tokens_to_ids_map[tuple(merged_list)])
+        else:
+            for ml in merged_list:
+                encoded_list.append(self.tokens_to_ids_map[tuple([ml])])
+
+        return encoded_list
+
+    def decode(self, input_ids: list[int]) -> str:
+        ids_to_tokens: list[int] = []
+
+        for iid in input_ids:
+            ids_to_tokens.extend(self.ids_to_tokens_map[iid])
+        byte_seq = bytes(ids_to_tokens)
+        return byte_seq.decode("utf-8")
 
     @classmethod
     def from_files(
@@ -48,10 +68,13 @@ class Tokenizer:
         | \s+                                # 7) other whitespace
         """
         token_re = re.compile(PAT, re.VERBOSE)
-        print(data)
-        tokenizer_map = {tuple(byte): token_id for token_id, byte in data.items()}
+        tokens_to_ids_map = {
+            tuple(byte): int(token_id) for token_id, byte in data.items()
+        }
+        ids_to_tokens_map = {int(token_id): byte for token_id, byte in data.items()}
         return cls(
-            tokenizer_map=tokenizer_map,
+            tokens_to_ids_map=tokens_to_ids_map,
+            ids_to_tokens_map=ids_to_tokens_map,
             special_tokens=special_tokens,
             token_re=token_re,
         )
